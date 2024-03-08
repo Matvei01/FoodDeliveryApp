d5ad09ffb21dd4d69bb40432e28c9f39be5e8977
Matvei <khlestov.matvei@mail.ru>
2024-03-08 23:33:21 +0500

    Added base new Roboto fonts

--- FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: -  UI Elements
+    private lazy var testLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 40, weight: UIFont.Weight(20))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    // MARK: -  Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemRed
+        view.addSubview(testLabel)
+        setConstraints()
+    }
+}
+
+// MARK: -  Private Methods
+private extension ViewController {
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            testLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            testLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            testLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20)
+        ])
     }
 }
 
