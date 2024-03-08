b0502d41d214ee2e9c17daba214eb36d7f705d27
Matvei <khlestov.matvei@mail.ru>
2024-03-08 22:49:16 +0500

    Created base project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Matvei Khlestov on 08.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
