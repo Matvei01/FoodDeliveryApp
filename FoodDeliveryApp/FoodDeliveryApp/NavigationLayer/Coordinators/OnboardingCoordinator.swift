a5728cc806d42bfcc946f1f8cf238e896358480c
Matvei <khlestov.matvei@mail.ru>
2024-03-15 14:02:39 +0500

    Added Factory

--- FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ FoodDeliveryApp/FoodDeliveryApp/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
+// MARK: - OnboardingCoordinator
 final class OnboardingCoordinator: Coordinator {
     
+    // MARK: - Properties
+    private let factory = SceneFactory.self
+    
+    // MARK: - Methods
     override func start() {
         showOnboarding()
     }
@@ -19,41 +24,10 @@ final class OnboardingCoordinator: Coordinator {
     }
 }
 
+// MARK: - Navigation
 private extension OnboardingCoordinator {
     func showOnboarding() {
-        var pages = [OnboardingPartViewController]()
-        
-        let firstVC = OnboardingPartViewController()
-        firstVC.imageToShow = .chickenLeg
-        firstVC.titleText = "Delicious Food"
-        firstVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        firstVC.buttonText = "Next"
-        
-        let secondVC = OnboardingPartViewController()
-        secondVC.imageToShow = .shipped
-        secondVC.titleText = "Fast Shipping"
-        secondVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum rhoncus nulla."
-        secondVC.buttonText = "Next"
-        
-        let thirdVC = OnboardingPartViewController()
-        thirdVC.imageToShow = .medal
-        thirdVC.titleText = "Certificate Food"
-        thirdVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris a id."
-        thirdVC.buttonText = "Next"
-        
-        let forthVC = OnboardingPartViewController()
-        forthVC.imageToShow = .creditCard
-        forthVC.titleText = "Payment Online"
-        forthVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui ultricies sit massa."
-        forthVC.buttonText = "Cool!"
-        
-        pages.append(firstVC)
-        pages.append(secondVC)
-        pages.append(thirdVC)
-        pages.append(forthVC)
-        
-        let presenter = OnboardingViewPresenter(coordinator: self)
-        let viewController = OnboardingViewController(pages: pages, viewOutput: presenter)
+        let viewController = factory.makeOnboardingScene(coordinator: self)
         navigationController?.pushViewController(viewController, animated: true)
     }
 }
